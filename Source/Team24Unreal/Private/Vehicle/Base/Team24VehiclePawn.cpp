77bbfb87a87c2d3124715ccd6f9e8b525753377d
sleeptired <musong0611@daum.net>
2026-05-12 18:08:40 +0900

    터널용 DELEGATE 구현

--- Source/Team24Unreal/Private/Vehicle/Base/Team24VehiclePawn.cpp
+++ Source/Team24Unreal/Private/Vehicle/Base/Team24VehiclePawn.cpp
@@ -295,4 +295,6 @@ void ATeam24VehiclePawn::SetInTunnel(bool bNewInTunnel)
 {
 	UE_LOG(LogTemp, Log, TEXT("SetInTunnel: %s"),
 		bNewInTunnel ? TEXT("true") : TEXT("false"));
+
+	OnTunnelToggleDelegate.Broadcast(bNewInTunnel);
 }
